0ccf7239787170d66582cfe9055b5c163ac2acb2
Sharbi <seanharbison001@gmail.com>
2019-07-22 12:42:19 +0100

    increase sample size

--- prepare_random_samples.py
+++ prepare_random_samples.py
@@ -4,11 +4,11 @@ from pyts.image import GramianAngularField
 from mpl_toolkits.axes_grid1 import ImageGrid
 import pickle as pkl
 
-random_input = np.random.randint(1, 300, (100000 ,4, 60))
+random_input = np.random.randint(1, 300, (1000000 ,4, 60))
 
 image_size = 32
 gasf = GramianAngularField(image_size, method='summation')
 X_gasf = [gasf.fit_transform(x) for x in random_input]
 
-pkl.dump(random_input, open('./data/output.pkl', 'wb'))
-pkl.dump(X_gasf[0], open('./data/input.pkl', 'wb'))
+pkl.dump(random_input, open('./data/output_1.pkl', 'wb'))
+pkl.dump(X_gasf[0], open('./data/input_1.pkl', 'wb'))
