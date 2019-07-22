c70e2f6dbd6020ee49b02162c7a3a2f2e59ad4fa
Sharbi <seanharbison001@gmail.com>
2019-07-22 17:49:38 +0100

    Added some printout to see stage of process when killed

--- prepare_random_samples.py
+++ prepare_random_samples.py
@@ -4,11 +4,16 @@ from pyts.image import GramianAngularField
 from mpl_toolkits.axes_grid1 import ImageGrid
 import pickle as pkl
 
+print("generating random input")
 random_input = np.random.randint(1, 300, (1000000 ,4, 60))
+print("Input generated")
+
 
 image_size = 32
+print("Performing GASF on input")
 gasf = GramianAngularField(image_size, method='summation')
 X_gasf = [gasf.fit_transform(x) for x in random_input]
+print("Finished GASF")
 
 pkl.dump(random_input, open('./data/output_1.pkl', 'wb'))
 pkl.dump(X_gasf[0], open('./data/input_1.pkl', 'wb'))
