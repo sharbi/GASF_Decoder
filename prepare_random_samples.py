bec97a135e7703219e288c2c9a31be56e93c5c3f
Sharbi <seanharbison001@gmail.com>
2019-07-22 19:34:22 +0100

    test input shape

--- prepare_random_samples.py
+++ prepare_random_samples.py
@@ -5,18 +5,18 @@ from mpl_toolkits.axes_grid1 import ImageGrid
 import pickle as pkl
 
 print("generating random input")
-random_input = np.random.randint(1, 300, (1000000 ,4, 60))
+random_input = np.random.randint(1, 300, (100 ,4, 60))
 print("Input generated")
 
 
 image_size = 32
 print("Performing GASF on input")
 gasf = GramianAngularField(image_size, method='summation')
-X_gasf = np.array((1000000, 4, 32, 32))
-for i, x in enumerate(random_input):
-    X_gasf[i] = gasf.fit_transform(x)
+X_gasf = [gasf.fit_transform(x) for x in random_input]
+
+print(X_gasf.shape)
 
 print("Finished GASF")
 
-pkl.dump(random_input, open('./data/output_1.pkl', 'wb'))
-pkl.dump(X_gasf[0], open('./data/input_1.pkl', 'wb'))
+np.save('./data/output.npy', random_input)
+np.save('./data/input_1.pkl', X_gasf[0])
