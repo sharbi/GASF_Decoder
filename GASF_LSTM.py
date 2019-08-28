0e28da3aef1ef28167ecc89a6a2ff6f13c43bac7
Sharbi <seanharbison001@gmail.com>
2019-08-28 17:00:05 +0100

    Updated input size

--- GASF_LSTM.py
+++ GASF_LSTM.py
@@ -3,8 +3,30 @@ import keras.backend as K
 from keras.layers import Input, Dense, LSTM, Reshape, TimeDistributed, RepeatVector
 from keras.models import Sequential
 import numpy as np
+import pandas as pd
 import math
 import h5py
+
+def one_hot_encode(sequence, n_unique):
+    encoding = list()
+    for value in sequence:
+        vector = [0 for _ in range(n_unique)]
+        vector[value] = 1
+        encoding.append(vector)
+    return array(encoding)
+
+def one_hot_decode(encoded_seq):
+    return [np.argmax(vector) for vector in encoded_seq]
+
+def to_supervised(seq, n_in, n_out):
+    df = pd.DataFrame(seq)
+    df = pd.concat([df.shift(n_in-i-1) for i in range(n_in)], axis=1)
+
+    values = df.values
+    width = seq.shape[1]
+    X = values.reshape(len(values), n_in, width)
+    y = values[:, 0:(n_out*width)].reshape(len(values), n_out, width)
+    return X, y
 
 
 if __name__ == '__main__':
@@ -13,14 +35,20 @@ if __name__ == '__main__':
     # define the hyperparameters
     epochs = 200
     batch_size = 256
-    input_shape = (4, 1024)
-    output_shape = (4, 60)
-    latent_dim = 256
+    input_shape = (1, 1024)
+    output_shape = (1, 60)
+    latent_dim = 150
+
+    n_in = 1024
+    n_out = 60
+    encoded_length =
+
 
     # define the model
     decoder = Sequential()
-    decoder.add(LSTM(latent_dim, input_shape=(4, 1024), return_sequences=True))
-    decoder.add(LSTM(latent_dim, return_sequences=True))
+    decoder.add(LSTM(latent_dim, input_shape=(batch_size, n_in, encoded_length), stateful=True)
+    decoder.add(RepeatVector())
+    decoder.add(LSTM(latent_dim, return_sequences=True, stateful=True))
     decoder.add(TimeDistributed(Dense(60)))
 
     decoder.compile(optimizer='rmsprop', loss='mse', metrics=['accuracy'])
@@ -30,7 +58,7 @@ if __name__ == '__main__':
     fout = h5py.File('./data/output.h5', 'r')
 
     X_input = np.array(list(fin['input']))
-    X_input = X_input.reshape(X_input.shape[0], 4, 1024)
+    X_input = X_input.reshape(X_input.shape[0], 1, 1024)
     y = np.array(list(fout['output']))
 
     fin.close()
