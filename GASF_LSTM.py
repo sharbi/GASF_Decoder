bc3b90bd1578fc5e59b3b623af66973a2de24bd9
Sharbi <seanharbison001@gmail.com>
2019-08-29 13:53:48 +0100

    Fixed batch input

--- GASF_LSTM.py
+++ GASF_LSTM.py
@@ -77,11 +77,7 @@ if __name__ == '__main__':
     config.gpu_options.allow_growth=True
 
     num_batches = num_train // batch_size
-
-
     epoch_decoder_loss = []
-
-    num_batches = X_input.shape[0] // batch_size
 
     for epoch in range(epochs):
 
@@ -89,8 +85,8 @@ if __name__ == '__main__':
 
 
 
-            input_batch = X_train[(num_batches * batch_size):(num_batches+1*batch_size)]
-            output_batch = y_train[(num_batches * batch_size):(num_batches+1*batch_size)]
+            input_batch = X_train[(i * batch_size):(i+1*batch_size)]
+            output_batch = y_train[(i * batch_size):(i+1*batch_size)]
             epoch_decoder_loss.append(decoder.train_on_batch(input_batch, output_batch))
             output = decoder.predict(input_batch)
             print(output)
