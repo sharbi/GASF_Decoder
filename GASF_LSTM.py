c3714d8569e32fbfd3e6a1795da182061df0d698
Sharbi <seanharbison001@gmail.com>
2019-09-06 05:55:13 +0100

    Fixed string conversion

--- GASF_LSTM.py
+++ GASF_LSTM.py
@@ -16,9 +16,9 @@ def string_to_length(len, strin):
 
 def convert_to_string(X, y, largest_in, largest_out):
     Xstr = [list(map(str, input)) for x in X for input in x]
-    Xstr = [list(map(partial(string_to_length(largest_in)), input)) for x in X for input in x]
+    Xstr = [tring_to_length(largest_in, number) for x in X for input in x for number in input]
     Ystr = [list(map(str, input)) for ys in y for input in y]
-    Ystr = [list(map(partial(string_to_length(largest_out)), input)) for ys in y for input in ys]
+    Ystr = [string_to_length(largest_out, number) for ys in y for input in ys for number in input]
 
     return Xstr, Ystr
 
