23a8280141888c41c7154246c31b0ede908b2395
Sharbi <seanharbison001@gmail.com>
2019-09-06 12:50:39 +0100

    Tried to reduce memory load from string conversion

--- GASF_LSTM.py
+++ GASF_LSTM.py
@@ -17,10 +17,10 @@ def string_to_length(len, strin):
 def convert_to_string(X, y, largest_in, largest_out):
     Xstr = [list(map(str, input)) for x in X for input in x]
     Xtr = np.array(Xstr)
-    Xstr = [string_to_length(largest_in, number) for x in X for input in x for number in input]
+    Xstr = [map(partial(string_to_length(largest_in)), input) for x in X for input in x]
     Ystr = [list(map(str, input)) for ys in y for input in y]
     Ystr = np.array(Ystr)
-    Ystr = [string_to_length(largest_out, number) for ys in y for input in ys for number in input]
+    Ystr = [map(partial(string_to_length(largest_out), input) for ys in y for input in ys]
 
     return Xstr, Ystr
 
