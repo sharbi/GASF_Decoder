94d9edcece5f234f27d9de6497c8bebb672143d7
Sharbi <seanharbison001@gmail.com>
2019-09-07 15:53:01 +0100

    Changed data preparation into prepare_random_samples to allow batch string conversion, stop memory overload

--- GASF_LSTM.py
+++ GASF_LSTM.py
@@ -11,64 +11,7 @@ from keras.utils.generic_utils import Progbar
 
 
 
-def string_to_length(max_len, strin):
-    strout = ''
-    strin = str(strin)
-    if len(strin) < max_len:
-        strout = ''.join([' ' for _ in range(max_len - len(strin))]) + strin
-    return strout
 
-def convert_to_string(X, y, largest_in, largest_out):
-    print("Input into string...")
-    Xstr = [string_to_length(largest_in, number) for x in X for input in x for number in input]
-    print("Example output:")
-    print(Xstr[0])
-    Ystr = list()
-    for i, set in enumerate(y):
-        progress_bar = Progbar(target=len(y))
-        for values in set:
-            patterns = list()
-            for value in values:
-                patterns.append(string_to_length(largest_out, value))
-            Ystr.append(patterns)
-        progress_bar.update(i + 1)
-
-
-    return Xstr, Ystr
-
-def integer_encode(X, y, alphabet):
-    char_to_int = dict((c, i) for i, c in enumerate(alphabet))
-    Xenc = list()
-    for i, pattern in enumerate(X):
-        progress_bar = Progbar(target=len(X))
-        integer_encoded = [char_to_int[char] for char in pattern]
-        Xenc.append(integer_encoded)
-        progress_bar.update(i+1)
-    Yenc = list()
-    for pattern in y:
-        integer_encoded = [char_to_int[char] for char in pattern]
-
-    return Xenc, Yenc
-
-def one_hot_encode(X, y, max_int):
-    Xenc = list()
-    for seq in X:
-        pattern = list()
-        for index in seq:
-            vector = [0 for _ in range(max_int)]
-            vector[index] = 1
-            pattern.append(vector)
-        Xenc.append(pattern)
-
-    Yenc = list()
-    for seq in y:
-        pattern = list()
-        for index in seq:
-            vector = [0 for _ in range(max_int)]
-            vector[index] = 1
-            pattern.append(vector)
-        Yenc.append(pattern)
-    return Xenc, Yenc
 
 # invert encoding
 def invert(seq, alphabet):
@@ -81,7 +24,6 @@ def invert(seq, alphabet):
 
 if __name__ == '__main__':
 
-    alphabet = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '-', ' ', 'e']
 
     # define the hyperparameters
     epochs = 200
@@ -112,23 +54,12 @@ if __name__ == '__main__':
     print("Loading data into array.")
 
     X_input = np.array(list(fin['input']))
-    X_input = X_input.reshape(X_input.shape[0], 1, 1024)
     y = np.array(list(fout['output']))
 
     fin.close()
     fout.close()
 
     print("Data loaded.")
-
-    print("Converting int to string.")
-    X_input, y = convert_to_string(X_input, y, largest_input, largest_output)
-    print("Converted, encoding to index and one_hot.")
-    X_input, y = integer_encode(X_input, y, alphabet)
-    X_input, y = one_hot_encode(X_input, y, len(alphabet))
-
-    print("All data modification completed.")
-
-    X_input, y = np.array(X_input), np.array(y)
 
     training_size = math.floor(0.75 * X_input.shape[0])
 
--- prepare_random_samples.py
+++ prepare_random_samples.py
@@ -3,9 +3,72 @@ from pyts.image import GramianAngularField
 import h5py
 import os
 
+def string_to_length(max_len, strin):
+    strout = ''
+    strin = str(strin)
+    if len(strin) < max_len:
+        strout = ''.join([' ' for _ in range(max_len - len(strin))]) + strin
+    return strout
+
+def convert_to_string(X, y, largest_in, largest_out):
+    print("Input into string...")
+    Xstr = [string_to_length(largest_in, number) for x in X for input in x for number in input]
+    print("Example output:")
+    print(Xstr[0])
+    Ystr = list()
+    for i, set in enumerate(y):
+        progress_bar = Progbar(target=len(y))
+        for values in set:
+            patterns = list()
+            for value in values:
+                patterns.append(string_to_length(largest_out, value))
+            Ystr.append(patterns)
+        progress_bar.update(i + 1)
+
+
+    return Xstr, Ystr
+
+def integer_encode(X, y, alphabet):
+    char_to_int = dict((c, i) for i, c in enumerate(alphabet))
+    Xenc = list()
+    for i, pattern in enumerate(X):
+        progress_bar = Progbar(target=len(X))
+        integer_encoded = [char_to_int[char] for char in pattern]
+        Xenc.append(integer_encoded)
+        progress_bar.update(i+1)
+    Yenc = list()
+    for pattern in y:
+        integer_encoded = [char_to_int[char] for char in pattern]
+
+    return Xenc, Yenc
+
+def one_hot_encode(X, y, max_int):
+    Xenc = list()
+    for seq in X:
+        pattern = list()
+        for index in seq:
+            vector = [0 for _ in range(max_int)]
+            vector[index] = 1
+            pattern.append(vector)
+        Xenc.append(pattern)
+
+    Yenc = list()
+    for seq in y:
+        pattern = list()
+        for index in seq:
+            vector = [0 for _ in range(max_int)]
+            vector[index] = 1
+            pattern.append(vector)
+        Yenc.append(pattern)
+    return Xenc, Yenc
 
 def generate_samples(n):
+    alphabet = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '-', ' ', 'e']
+
     for _ in range(n):
+
+        X, y = list()
+
         print("generating random input")
         random_input = np.random.randint(1, 300, (1000 ,1, 60))
         print("Input generated")
@@ -20,28 +83,34 @@ def generate_samples(n):
 
         print("Finished GASF")
 
+        X_gasf = X_gasf.reshape(X_gasf.shape[0], 1, 1024)
+
+        X, y = convert_to_string(X_gasf, random_input, 22, 3)
+        X, y = integer_encode(X, y, alphabet)
+        X, y = one_hot_encode(X, y, len(alphabet))
+
         out_directory = './data/'
         output_file = 'output.h5'
         input_file = 'input.h5'
 
         if not os.path.exists(out_directory + output_file):
             fout = h5py.File(out_directory + output_file, 'w')
-            dset_out = fout.create_dataset("output", data=random_input, chunks=True, maxshape=(None, 1, 60))
+            dset_out = fout.create_dataset("output", data=y, chunks=True, maxshape=(None, 1, 60))
 
         else:
             fout = h5py.File(out_directory + output_file, 'a')
-            fout['output'].resize((fout['output'].shape[0] + random_input.shape[0]), axis=0)
-            fout['output'][-random_input.shape[0]:] = random_input
+            fout['output'].resize((fout['output'].shape[0] + y.shape[0]), axis=0)
+            fout['output'][-random_input.shape[0]:] = y
 
 
         if not os.path.exists(out_directory + input_file):
             fin = h5py.File(out_directory + input_file, 'w')
-            dset_in = fin.create_dataset("input", data=X_gasf, chunks=True, maxshape=(None, 1, 32, 32))
+            dset_in = fin.create_dataset("input", data=X, chunks=True, maxshape=(None, 1, 32, 32))
 
         else:
             fin = h5py.File(out_directory + input_file, 'a')
-            fin['input'].resize((fin['input'].shape[0] + X_gasf.shape[0]), axis=0)
-            fin['input'][-X_gasf.shape[0]:] = X_gasf
+            fin['input'].resize((fin['input'].shape[0] + X.shape[0]), axis=0)
+            fin['input'][-X_gasf.shape[0]:] = X
 
 
 
@@ -49,4 +118,4 @@ def generate_samples(n):
         fin.close()
 
 if __name__ == '__main__':
-    generate_samples(100)
+    generate_samples(250)
