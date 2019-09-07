077f15fdeeca72ceaeeb1b51a5266cf603c359e4
Sharbi <seanharbison001@gmail.com>
2019-09-07 15:56:04 +0100

    Removed progbar

--- prepare_random_samples.py
+++ prepare_random_samples.py
@@ -59,7 +59,7 @@ def one_hot_encode(X, y, max_int):
     return Xenc, Yenc
 
 def generate_samples(n):
-    alphabet = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '-', ' ', 'e']
+    alphabet = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '-', ' ', 'e', '']
 
     for _ in range(n):
 
