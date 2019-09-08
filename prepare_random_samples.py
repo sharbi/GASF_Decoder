73c826aa6dcbf73479c172f7bb07f0cf694230cc
Sharbi <seanharbison001@gmail.com>
2019-09-08 23:25:09 +0100

    Testing string conversion functions - currently outputs wrong shape

--- prepare_random_samples.py
+++ prepare_random_samples.py
@@ -69,11 +69,10 @@ def one_hot_encode(X, y, max_int):
             for index in seq:
                 vector = [0 for _ in range(max_int)]
                 vector[index] = 1
+                print(len(vector))
                 pattern.append(vector)
+            print(len(pattern))
             final_in.append(pattern)
-        if len(final_in) != 1024:
-            print("ERROR")
-            quit()
         Xenc.append(final_in)
     Yenc = list()
     for patterns in y:
