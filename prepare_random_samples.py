fbf6ac15bd8f6147383229f1a8812cd35422125e
Sharbi <seanharbison001@gmail.com>
2019-09-09 13:50:17 +0100

    Added one hot encoding

--- prepare_random_samples.py
+++ prepare_random_samples.py
@@ -142,7 +142,7 @@ def generate_samples(n):
 
         if not os.path.exists(out_directory + output_file):
             fout = h5py.File(out_directory + output_file, 'w')
-            dset_out = fout.create_dataset("output", data=y, chunks=True, maxshape=(None, 60, 3))
+            dset_out = fout.create_dataset("output", data=y, chunks=True, maxshape=(None, 60, 3, 14))
 
         else:
             fout = h5py.File(out_directory + output_file, 'a')
@@ -152,7 +152,7 @@ def generate_samples(n):
 
         if not os.path.exists(out_directory + input_file):
             fin = h5py.File(out_directory + input_file, 'w')
-            dset_in = fin.create_dataset("input", data=X, chunks=True, maxshape=(None, 1024, 23))
+            dset_in = fin.create_dataset("input", data=X, chunks=True, maxshape=(None, 1024, 23, 14))
 
         else:
             fin = h5py.File(out_directory + input_file, 'a')
